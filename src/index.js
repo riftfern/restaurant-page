2d45e5c53e6adf6f23166988a121553d255d2786
riftfern <adriftfern@gmail.com>
2022-10-16 17:24:48 -0400

    added background and styled page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import './styles.css';
-import Icon from './revolt.jpg'
+
+
 
 function content(){
     const element = document.createElement('div');
+    element.className = 'element';
 
-    //create document header
+    
     const header = document.createElement('h1');
-    header.innerText = 'The New Trendy Taco Place Your Friend Told You To Try';
-
+    header.innerText = 'Taqueria Mal Dia';
     element.appendChild(header);
 
-    //add background to div
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    const bodyDiv = document.createElement('div');
+    bodyDiv.className = 'bodyDiv';
+    const quote = document.createElement('h2');
+    quote.className = 'quote';
+    const originStory = document.createElement('div');
+    originStory.className = 'originStory';
 
-    element.appendChild(myIcon);
+    quote.innerText = `The Trendy New Taco Place Your Friends Swear Isn't Overpriced and Pretentious`;
+    originStory.innerText = `Founded six months into the pandemic, after the local family that owned it for years defaulted on their mortgage.  Taqueria Mal Dia strives to bring you authentic flavors, in an atmosphere that makes you feel shame for living in a first world country.`;
+    
+    bodyDiv.appendChild(quote);
+    bodyDiv.appendChild(originStory);
+    element.appendChild(bodyDiv);
+
+    
+
+    const footer = document.createElement('footer');
+    footer.innerText = `We reserve the right to add as many 20% service charges as we see fit.  We accept cash but you must round up to the nearest twenty, and we do not give change.`;
+    element.appendChild(footer);
+
+    //add background to div
+    //const myIcon = new Image();
+    //myIcon.src = Icon;
+    //element.style.backgroundImage = myIcon;
+    
 
     return element;
 
 }
+
 
 document.body.appendChild(content());
